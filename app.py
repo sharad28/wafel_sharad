c607e903ef52bc8a607891e93886174dae1c7035
sharadmishra28 <53832290+sharadmishra28@users.noreply.github.com>
2021-11-20 21:40:22 +0530

    try with app.run instead of simple_server

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ port = int(os.getenv("PORT", 5001))
 
 if __name__ == "__main__":
     host = '0.0.0.0'
-    # app.run()
-    httpd = simple_server.make_server(host=host, port=port, app=app)
+    app.run(host=host,port=port)
+    # httpd = simple_server.make_server(host=host, port=port, app=app)
     # httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
     # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
