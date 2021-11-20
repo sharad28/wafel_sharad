37b5776945880b016b33a44d330709fe5d8f0570
sharadmishra28 <53832290+sharadmishra28@users.noreply.github.com>
2021-11-20 20:46:41 +0530

    test3

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index1():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "CICD is running1"
+    return "CICD is running2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
